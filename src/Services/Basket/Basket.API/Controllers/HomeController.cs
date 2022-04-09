8bc5f66689b57f1af7cfd35beabe93107a1956dc
Sheeraz Ahmad <ahmed.sheeraz1@gmail.com>
2022-04-09 12:30:20 +0530

    added controller

---
+++ src/Services/Basket/Basket.API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
