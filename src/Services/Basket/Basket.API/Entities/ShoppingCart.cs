d168e32fd3c56ac14dd5245e779be1675bbc84fb
Sheeraz Ahmad <ahmed.sheeraz1@gmail.com>
2022-04-09 12:22:49 +0530

    Basket API adding configuration

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public decimal TotalPrice
+        {
+
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    totalPrice = item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddStackExchangeRedisCache(options =>
+//{
+//    options.Configuration=localhost:6379
 
+//});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
